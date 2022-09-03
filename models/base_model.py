7c26a317e72599d63a73bcb229d0bbe1f2ff38e0
antish <AntenehShi@ethiopianairlines.com>
2022-09-03 11:49:24 +0300

    linting for task  3

--- models/base_model.py
+++ models/base_model.py
@@ -1,27 +1,30 @@
 #!/usr/bin/python3
+"""Base class file."""
+
 import uuid
 from datetime import datetime
 
 
 class BaseModel:
+    """BaseModel."""
 
     def __init__(self, *args, **kwargs):
-        """Initialize the instance of class"""
+        """Initialize the instance of class."""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
-        """str definition"""
+        """Str definition."""
         clsname = self.__class__.__name__
         return "[{}] ({}) <{}>".format(clsname, self.id, self.__dict__)
 
     def save(self):
-        """save definition"""
+        """Save definition."""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__ of the instance"""
+        """Return a dictionary containing all keys/values of __dict__ of the instance."""
         dict_repr = {}
         for key, value in self.__dict__.items():
             dict_repr[key] = value
